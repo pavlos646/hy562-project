a52173cb79dbcbf9c14abe90e1d82fc137d52a14
pavlos646 <1pagiamani@gmail.com>
2026-01-28 22:02:04 +0200

    ARM

--- src/main.py
+++ src/main.py
@@ -1,14 +1,123 @@
+import os
+import json
+from dotenv import load_dotenv
+from pyspark.ml.fpm import FPGrowth
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, collect_set, size
 
+spark = None
+neo4j_url = "bolt://[0:0:0:0:0:0:0:0]:7687"
+neo4j_user = "neo4j"
+neo4j_pass = "password"
+
+
+def get_supporting_subgraph(rules_df, limit=10):
+    node_list = [1,2,3]
+
+    cypher_query = f"""
+        MATCH p = (n)-[*1..2]-(m)
+        WHERE id(n) IN {node_list} 
+            AND id(m) IN {node_list}
+        RETURN p
+    """
+    subgraph_df = execute_query(cypher_query)
+
+    return subgraph_df
+
+
+def execute_query(query):
+    return spark.read \
+        .format("org.neo4j.spark.DataSource") \
+        .option("url", neo4j_url) \
+        .option("authentication.type", "basic") \
+        .option("authentication.basic.username", neo4j_user) \
+        .option("authentication.basic.password", neo4j_pass) \
+        .option("query", query) \
+        .load()
+
+
+def filter_out_god_nodes(raw_baskets):
+    # import pandas as pd
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    # from mlxtend.preprocessing import TransactionEncoder
+    # from mlxtend.frequent_patterns import fpgrowth
+
+    # 1. Your raw baskets from the Neo4j query
+    # raw_baskets = [
+    #     ["Luke", "R2-D2", "X-Wing", "Tatooine"],
+    #     ["Han", "R2-D2", "Falcon", "Tatooine"],
+    #     ["Leia", "R2-D2", "Falcon", "Alderaan"],
+    #     ["Luke", "Yoda", "X-Wing", "Dagobah"]
+    # ]
+
+    # 2. Convert baskets to "sentences" for TfidfVectorizer
+    # We join names with underscores to keep "Darth Vader" as one token
+    sentences = [" ".join([item.replace(" ", "_") for item in basket]) for basket in raw_baskets]
+
+    raw_baskets.foreach(" ".join([item.replace(" ", "_") for item in basket]))
+
+    # 3. Use max_df to automatically kill "God Nodes"
+    # max_df=0.7 means "remove items that appear in more than 70% of baskets"
+    tfidf = TfidfVectorizer(max_df=0.7, token_pattern=r"(?u)\b\w+\b")
+    tfidf_matrix = tfidf.fit_transform(sentences)
+
+    print("TF_IDF Matrix:")
+    print(tfidf_matrix)
+
+    # Get the list of "Meaningful" items (the ones that survived)
+    survived_items = set(tfidf.get_feature_names_out())
+
+    # 4. Rebuild the baskets using only survivors
+    clean_baskets = [
+        [item.replace(" ", "_") for item in basket if item.replace(" ", "_") in survived_items]
+        for basket in raw_baskets
+    ]
+
+    for i in range(15):
+        print(f"{i+1}: {clean_baskets[i]}")
+    
+
+    # MAYBE: 5. Run FP-Growth on the cleaned data
+    # te = TransactionEncoder()
+    # te_ary = te.fit(clean_baskets).transform(clean_baskets)
+    # df = pd.DataFrame(te_ary, columns=te.columns_)
+
+    # frequent_itemsets = fpgrowth(df, min_support=0.4, use_colnames=True)
+    # print(frequent_itemsets)
+
+
+    #----------------------
+    from pyspark.ml.feature import HashingTF, IDF
+
+    # 1. Calculate Term Frequency
+    hashingTF = HashingTF(inputCol="items", outputCol="rawFeatures")
+    tf_df = hashingTF.transform(baskets_df)
+
+    # 2. Calculate Inverse Document Frequency
+    idf = IDF(inputCol="rawFeatures", outputCol="features")
+    idfModel = idf.fit(tf_df)
+    rescale_df = idfModel.transform(tf_df)
+
 def main():
-    neo4j_url = "bolt://[0:0:0:0:0:0:0:0]:7687"
-    neo4j_user = "neo4j"
-    neo4j_pass = "password"
+    global spark
+
+    load_dotenv()
+    GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+
+    # from google import genai
+    # client = genai.Client()
+
+    # response = client.models.generate_content(
+    #     # model="gemini-3-flash-preview",
+    #     model="gemini-2.0-flash", 
+    #     contents="Explain why the sky is blue."
+    # )
+    # print(response.text)
 
     spark = SparkSession.builder \
         .appName("HY562-Step2-Neo4j-To-Baskets") \
         .master("local[*]") \
+        .config("spark.driver.memory", "8g") \
         .config("spark.jars.packages", "org.neo4j:neo4j-connector-apache-spark_2.13:5.4.0_for_spark_3") \
         .getOrCreate()
 
@@ -16,27 +125,47 @@ def main():
 
     print(f"Connecting to {neo4j_url}...")
 
-    df = spark.read \
-        .format("org.neo4j.spark.DataSource") \
-        .option("url", neo4j_url) \
-        .option("authentication.type", "basic") \
-        .option("authentication.basic.username", neo4j_user) \
-        .option("authentication.basic.password", neo4j_pass) \
-        .option("relationship", "APPEARED_IN") \
-        .option("relationship.source.labels", ":Character") \
-        .option("relationship.target.labels", ":Film") \
-        .load()
+    # TODO: getting  the info dynamically.
+    # df.show(20, truncate=False)
 
-    baskets = df.groupBy(col("`target.title`").alias("film")) \
-        .agg(collect_set(col("`source.name`")).alias("items")) \
-        .filter(size(col("items")) > 1)
+    cypher_query = """
+    MATCH (n)-[r]-()
+    RETURN id(n) AS node_id, count(r) AS freq
+    ORDER BY freq DESC
+    """
+    df = execute_query(cypher_query)
+    df.show(20, truncate=False)
+    print(f"ITEM COUNT: {df.distinct().count()}")
 
-    print("Baskets (film -> items):")
-    baskets.show(20, truncate=False)
 
-    print(f"Number of baskets: {baskets.count()}")
+    # MAYBE: add WHERE id(s) < id(t) to avoid having both [A, B] and [B, A]
+    cypher_query = """
+        MATCH (s)--(t)
+        WITH s, collect(DISTINCT toString(id(t))) AS neighbors
+        WHERE size(neighbors) > 1 AND size(neighbors) < 10
+        RETURN neighbors + toString(id(s)) AS items
+    """
+    
+    df = execute_query(cypher_query)
+    
+    # Optional: Cache the DF because FPGrowth reads it multiple times
+    df.cache()
 
-    baskets.write.mode("overwrite").json("output/baskets_json")
+    # TODO: more dynamically find minsupport
+    total_count = df.count()
+    
+    print(f"Total Transactions: {total_count}")
+    fp = FPGrowth(itemsCol="items", minSupport=0.1, minConfidence=0.5)
+    model = fp.fit(df)
+
+    print("Frequent itemsets (Sorted by least frequent):")
+    # 3. Sorting by "freq" (default is ascending) shows the rare items first
+    model.freqItemsets.sort("freq").show(100)
+    # print("Association rules:")
+    model.associationRules.show(truncate=False)
+
+    # tmp = get_supporting_subgraph(model.associationRules)
+    # tmp.show(truncate=False)
 
     spark.stop()
 
